bbc8c3b7a2aaf91d43ca56ddbdafe64d0b70a5df
RayenEssid <rayenessid15@gmaul.com>
2025-12-27 00:24:10 +0100

    amelierationproject1

--- src/app/Services/formateur-service.service.ts
+++ src/app/Services/formateur-service.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Formateur } from '../Models/formateur';
-
+//backend
 @Injectable({
   providedIn: 'root'
 })
